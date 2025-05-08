a4ec83a71b61b85e3668de68bc2c2c081c6ffab0
sultantemuruly <sultantemuruly@gmail.com>
2025-05-08 22:10:52 +0500

    feat: create basic ai agent

---
+++ agents/agent.py
@@ -0,0 +1,25 @@
+from smolagents import ToolCallingAgent, ToolCollection, LiteLLMModel
+from mcp import StdioServerParameters
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+api_key = os.getenv("OPENAI_API_KEY")
+
+model = LiteLLMModel(
+    model_id="gpt-4o",
+    api_key=api_key,
+)
+
+server_parameters = StdioServerParameters(
+    command="uv",
+    args=["run", "../server/server.py"],
+    env=None,
+)
+
+with ToolCollection.from_mcp(
+    server_parameters, trust_remote_code=True
+) as tool_collection:
+    agent = ToolCallingAgent(tools=[*tool_collection.tools], model=model)
+    agent.run("What was Apple's last stock price?")
